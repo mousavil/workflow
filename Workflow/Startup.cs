04e984118fba3677f1b013a6d6ec91f5e25eabc4
Amir <ammaam308@gmail.com>
2020-11-16 14:31:28 -0500

    commit

--- Workflow/Startup.cs
+++ Workflow/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -35,18 +36,21 @@ namespace Workflow
                 //options.EnableSensitiveDataLogging();
             });
             services.AddScoped(typeof(IBaseRepository<,>), typeof(BaseRepository<,>));
+
             
-            //services.AddCors(options =>
-            //{
-            //    options.AddPolicy("EnabledCores", builder =>
-            //    {
-            //        builder.AllowAnyOrigin()
-            //            .AllowAnyHeader()
-            //            .AllowAnyMethod()
-            //            .AllowCredentials()
-            //            .Build();
-            //    });
-            //});
+            services.AddMvc(option => option.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowMyOrigin", builder =>
+                {
+                    builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials()
+                        .Build();
+                });
+            });
 
             services.AddSwaggerGen(c =>
             {
@@ -70,11 +74,11 @@ namespace Workflow
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            app.UseCors("AllowMyOrigin");
 
+            
             app.UseRouting();
 
-            //app.UseCors("EnabledCores");
 
             app.UseEndpoints(endpoints =>
             {
